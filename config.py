45dd451f9fd05633decb3ef270d491cbbfb5efca
akmoviebot <mrak7753@gmail.com>
2025-06-04 15:02:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ APP_ID = int(os.environ.get("APP_ID", "25471015"))
 API_HASH = os.environ.get("API_HASH", "0ab2955ad5b1a913e220800b5fc5db36")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002249219491"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002357498682"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "7459282233"))
@@ -30,7 +30,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ankushdeb91:WpbkXkwE0rL98
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002200165721"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", False)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
